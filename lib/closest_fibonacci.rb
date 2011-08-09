79fcab6a58fd632c021fe4486fd6fa57862043eb
Rahul <rahul.baxi@sv.cmu.edu>
2011-08-08 20:24:07 -0700

    Edited lib/closest_fibonacci.rb via GitHub

--- lib/closest_fibonacci.rb
+++ lib/closest_fibonacci.rb
@@ -2,7 +2,7 @@ Integer.class_eval do
   def closest_fibonacci
   arr = Array.new
   i1, i2, i = 1, 1, 0 
-    while i1 <= self
+    while i1 < self
       arr[i] = i1
       i=i+1
       i1, i2 = i2, i1+i2
